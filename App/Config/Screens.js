e84b3dd7b3e96cf2dd42aa70b2443bfcdb2c1734
ace68723 <ace68723@gmail.com>
2018-01-02 13:38:35 -0500

    cm_restaurant init

---
+++ App/Config/Screens.js
@@ -0,0 +1,21 @@
+import { Navigation } from 'react-native-navigation';
+
+import AboutUs from '../Components/AboutUs';
+import CreateOrder from '../Components/CreateOrder';
+import CreateOrderDetail from '../Components/CreateOrderDetail';
+import Login from '../Components/Login';
+import OrderDetail from '../Components/OrderDetail';
+import OrderHistory from '../Components/OrderHistory';
+import PaymentHistory from '../Components/PaymentHistory';
+
+// register all screens of the app (including internal ones)
+export function registerScreens() {
+  Navigation.registerComponent('AboutUs', () => AboutUs);
+  Navigation.registerComponent('CreateOrder', () => CreateOrder);
+  Navigation.registerComponent('CreateOrderDetail', () => CreateOrderDetail);
+  Navigation.registerComponent('Login', () => Login);
+  Navigation.registerComponent('OrderDetail', () => OrderDetail);
+  Navigation.registerComponent('OrderHistory', () => OrderHistory);
+  Navigation.registerComponent('PaymentHistory', () => PaymentHistory);
+
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+import { Navigation } from 'react-native-navigation';
+
+import { registerScreens } from './App/Config/Screens';
+
+registerScreens();
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'Login',
+    navigatorStyle: {
+      navBarHidden: false,
+    },
+    title:'Login',
+    appStyle: {
+     navBarTextColor:"#c49a6c",
+   },
+    navigatorButtons: {}
+  },
+  passProps: {},
+  animationType: 'slide-down'
+});
