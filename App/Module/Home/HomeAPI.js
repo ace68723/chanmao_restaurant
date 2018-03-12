9ad5041d52f2400aee7c74b4c8c8f82a71a7f563
ricardol01 <mgu@yorku.ca>
2018-03-12 15:05:54 -0400

    cancel fetch

--- App/Module/Home/HomeAPI.js
+++ App/Module/Home/HomeAPI.js
@@ -1,4 +1,5 @@
 import { API_ORDERHANDLE,API_FETCHORDER } from '../../Config/API';
+import fetch_cancel from './react-native-cancelable-fetch';
 export default  {
     orderHandle(token,rid,task,items,oid){
       const url = API_ORDERHANDLE;
@@ -29,7 +30,30 @@ export default  {
              throw error
         })
      },
+
+
+
+
+
      fetchOrder(authtoken) {
+       const makeCancelable = (promise) => {
+         let hasCanceled_ = false;
+         const wrappedPromise = new Promise((resolve, reject) => {
+           promise.then((val) =>
+             hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
+           );
+           promise.catch((error) =>
+             hasCanceled_ ? reject({isCanceled: true}) : reject(error)
+           );
+         });
+         return {
+           promise: wrappedPromise,
+           cancel() {
+             hasCanceled_ = true;
+           },
+         };
+       };
+
         const url = API_FETCHORDER;
         let options = {
             method: 'GET',
@@ -40,11 +64,64 @@ export default  {
             }
         }
 
-      return fetch(url,options)
-           .then((response) => response.json())
-           .catch((error) => {
-               console.log(error);
-               throw error
-          })
+        setTimeout(()=> {
+
+          fetch_cancel.abort(1);
+          console.log('abort');
+        }, 30000);
+
+        return fetch_cancel(url,options,1)
+         .then((response) => response.json())
+         .catch((error) => {
+             console.log(error);
+             throw error
+        })
+
+
+      // const result= fetch(url,options)
+      //      .then((response) => response.json())
+      //      .catch((error) => {
+      //          console.log(error);
+      //          throw error
+      //     })
+      //     const cancelable = makeCancelable(result);
+      //
+      //
+      //
+      //     setTimeout(function () {
+      //       cancelable.cancel();
+      //     }, 30000);
+      //     return result;
+
+          // this.cancelable = makeCancelable(fetch(url,options));
+          //         return(this.cancelable.promise
+          //             .then((response)=>response.json())
+          //             .catch((error)=> {
+          //                 console.log(error);
+          //                 throw error
+          //             }));
+          //
+          //   this.cancelable.cancel();
+
+
+
+
+
+      // let cancelable = makeCancelable(fetch(url,options));
+      //         const result= this.cancelable.promise
+      //             .then((response)=>response.json())
+      //             .catch((error)=> {
+      //                 console.log(error);
+      //                 throw error
+      //             });
+      //
+      //
+      //
+      // setTimeout(function () {
+      //   cancelable.cancel();
+      // }, 3);
+      // return result;
+
+
      }
   }
