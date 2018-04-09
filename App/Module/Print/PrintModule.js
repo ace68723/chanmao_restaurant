c48ab3874345582930d8254c266c9ced05d80cb1
ricardol01 <mgu@yorku.ca>
2018-04-09 15:42:15 -0400

    print left align

--- App/Module/Print/PrintModule.js
+++ App/Module/Print/PrintModule.js
@@ -59,9 +59,9 @@ export default  {
                 // await NativeModules.customAndroid.printOriginalText("    "+data.orderArray[i].ds_name);
                 // await NativeModules.customAndroid.printOriginalText("    "+data.orderArray[i].amount);
                 // await NativeModules.customAndroid.printOriginalText("    "+data.orderArray[i].price);
-                await NativeModules.customAndroid.printLine(data.orderArray[i].int_no,data.orderArray[i].ds_name,data.orderArray[i].amount+' x '+data.orderArray[i].price,1,2,2,0,1,2);
+                await NativeModules.customAndroid.printLine(data.orderArray[i].int_no,data.orderArray[i].ds_name,data.orderArray[i].amount+' x '+data.orderArray[i].price,1,2,2,0,0,2);
                 for(let j = 0; j < data.orderArray[i].tps.length; j++){
-                  await NativeModules.customAndroid.printLine('',data.orderArray[i].tps[j].tp_name,data.orderArray[i].tps[j].amount+' x '+data.orderArray[i].tps[j].price,1,2,2,0,1,2);
+                  await NativeModules.customAndroid.printLine('',data.orderArray[i].tps[j].tp_name,data.orderArray[i].tps[j].amount+' x '+data.orderArray[i].tps[j].price,1,2,2,0,0,2);
                 }
 
              }
