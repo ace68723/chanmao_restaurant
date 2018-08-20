cd8854a81c6bb5e5303696a0ba058590eb3122bd
ricardol01 <mgu@yorku.ca>
2018-08-20 16:48:33 -0400

    printing

--- android/app/src/main/java/com/xm_restaurant/customAndroid.java
+++ android/app/src/main/java/com/xm_restaurant/customAndroid.java
@@ -238,7 +238,7 @@ public class customAndroid extends ReactContextBaseJavaModule {
         try {
 
             Log.i("kaltin", "printTable: "+tableItem.getText()[0]+tableItem.getText()[1]+tableItem.getText()[2]);
-            woyouService.printColumnsString(tableItem.getText(), tableItem.getWidth(), tableItem.getAlign(), null);
+            woyouService.printColumnsText(tableItem.getText(), tableItem.getWidth(), tableItem.getAlign(), null);
 
             woyouService.lineWrap(1, null);
         } catch (RemoteException e) {
